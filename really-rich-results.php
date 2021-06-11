39200e69a1a8d9b49cbbfd9d613b816a26f22017
JeffMatsonPagely <jeff.matson@pagely.com>
2021-06-11 11:25:54 -0400

    Version bump

--- really-rich-results.php
+++ really-rich-results.php
@@ -3,15 +3,15 @@
  * Main plugin file for bootstrapping Really Rich Results.
  *
  * @link              https://github.com/pagely/really-rich-results
- * @since             0.1.2
+ * @since             0.1.3
  * @package           Really_Rich_Results
  *
  * @wordpress-plugin
  * Plugin Name:       Really Rich Results
  * Plugin URI:        https://github.com/pagely/really-rich-results
  * Description:       Adds JSON-LD output for structured data
- * Version:           0.1.2
- * Author:            JeffMatson, joshuastrebel, Pagely
+ * Version:           0.1.3
+ * Author:            JeffMatson
  * Author URI:        https://pagely.com
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
@@ -24,7 +24,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'RRR_VERSION', '0.1.2' );
+define( 'RRR_VERSION', '0.1.3' );
 define( 'RRR_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
 define( 'RRR_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
 
