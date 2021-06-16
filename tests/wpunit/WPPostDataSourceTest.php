d5f5caf1a70e4f1815509a56249dbc1fbbec678f
JeffMatsonPagely <jeff.matson@pagely.com>
2021-06-16 11:57:27 -0400

    Moar integration tests

--- tests/wpunit/WPPostDataSourceTest.php
+++ tests/wpunit/WPPostDataSourceTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use Really_Rich_Results\Data_Sources\WP_Comment;
+use Really_Rich_Results\Data_Sources\Generic;
+use Really_Rich_Results\Data_Sources\WP_User;
 use Really_Rich_Results\Factories\Data_Source as Data_Source_Factory;
 
 class WPPostDataSourceTest extends \Codeception\TestCase\WPTestCase
@@ -10,61 +12,152 @@ class WPPostDataSourceTest extends \Codeception\TestCase\WPTestCase
      */
     protected $tester;
 
-    protected $postDataSource;
+    private $commentCount = 5;
 
-    protected $createdPost;
-    
     public function setUp(): void {
-        // Before...
         parent::setUp();
-
-        // Set up post data source.
-        $this->createdPost = static::factory()->post->create_and_get();
-        $this->postDataSource = Data_Source_Factory::create( $this->createdPost );
     }
 
     public function tearDown(): void {
-        // Reset post data source.
-        $this->postDataSource = null;
-        $this->createdPost = null;
-
-        // Then...
         parent::tearDown();
     }
 
-    public function testInvalidProp() {
-        $this->assertNull( $this->postDataSource->get_schema_property( 'invalid' ) );
+    private function createPost( $with = array() ) {
+        $post = static::factory()->post->create_and_get( array(
+            'post_type'   => 'post',
+            'post_author' => 1,
+        ));
+
+        if ( in_array( 'comments', $with ) ) {
+            static::factory()->comment->create_post_comments( $post->ID, $this->commentCount );
+        }
+
+        if ( in_array( 'featured_image', $with ) ) {
+            $attachment = static::factory()->attachment->create_upload_object( codecept_data_dir() . 'test-image.png', $post->ID );
+            set_post_thumbnail( $post->ID, $attachment );
+        }
+
+        return Data_Source_Factory::create( $post );
     }
 
-    public function testIdProp() {
-        $postId = $this->createdPost->ID;
-        $this->assertEquals( $this->postDataSource->get_schema_property('@id'), $postId );
+    public function testInvalidSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertNull( $dataSource->get_schema_property('invalid_prop_name') );
     }
 
-    public function testHeadlinePropIsTitle() {
-        $postTitle = apply_filters( 'the_title', $this->createdPost->post_title );
-        $this->assertEquals( $this->postDataSource->get_schema_property('headline'), $postTitle );
+    public function testDefaultSchemaType() {
+        $dataSource = $this->createPost();
+        $this->assertEquals( 'Article', $dataSource->get_schema_type() );
     }
 
-    public function testAbstractPropIsExcerpt() {
-        $excerpt = get_the_excerpt( $this->createdPost );
-        $this->assertEquals( $this->postDataSource->get_schema_property('abstract'), $excerpt );
+    public function testShowAuthor() {
+        $dataSource = $this->createPost();
+        $this->assertTrue( $dataSource->show_author() );
     }
 
-    public function testDescriptionPropIsExcerpt() {
-        $excerpt = get_the_excerpt( $this->createdPost );
-        $this->assertEquals( $this->postDataSource->get_schema_property('description'), $excerpt );
+    public function testIdSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsNumeric( $dataSource->get_schema_property('@id') );
     }
 
-    public function testCommentsPropIsValid() {
-        $comment_count = 5;
-        $post = static::factory()->post->create_and_get();
-        static::factory()->comment->create_post_comments( $post->ID, $comment_count );
-        
-        $data_source = Data_Source_Factory::create( $post );
-        $comments_prop = $data_source->get_schema_property('comment');
+    public function testAbstractSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsString( $dataSource->get_schema_property('abstract') );
+    }
 
-        $this->assertCount( $comment_count, $comments_prop );
-        $this->assertContainsOnlyInstancesOf( WP_Comment::class, $comments_prop );
+    public function testArticleBodySchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsString( $dataSource->get_schema_property('articleBody') );
+    }
+
+    public function testArticleSectionSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertContainsOnly( 'string', $dataSource->get_schema_property('articleSection') );
+    }
+
+    public function testAuthorSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertInstanceOf( WP_User::class, $dataSource->get_schema_property('author') );
+    }
+
+    public function testCommentSchemaProp() {
+        $dataSource = $this->createPost( array( 'comments' ) );
+        $comments = $dataSource->get_schema_property('comment');
+
+        $this->assertCount( $this->commentCount, $comments );
+        $this->assertContainsOnlyInstancesOf( WP_Comment::class, $comments );
+    }
+
+    public function testCommentCountSchemaProp() {
+        $dataSource = $this->createPost( array( 'comments' ) );
+        $this->assertEquals( $this->commentCount, $dataSource->get_schema_property('commentCount') );
+    }
+
+    public function testDateCreatedSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsString( $dataSource->get_schema_property('dateCreated') );
+    }
+
+    public function testDateModifiedSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsString( $dataSource->get_schema_property('dateModified') );
+    }
+
+    public function testDatePublishedSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsString( $dataSource->get_schema_property('datePublished') );
+    }
+
+    public function testDescriptionSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsString( $dataSource->get_schema_property('description') );
+    }
+
+    public function testDiscussionUrlSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertNull( $dataSource->get_schema_property('discussionUrl') );
+
+        $dataSource = $this->createPost(array('comments'));
+        $this->assertIsString( $dataSource->get_schema_property('discussionUrl') );
+    }
+
+    public function testHeadlineSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsString( $dataSource->get_schema_property('headline') );
+    }
+
+    public function testImageSchemaProp() {
+        $dataSource = $this->createPost( array('featured_image') );
+        $this->assertIsString( $dataSource->get_schema_property('image') );
+    }
+
+    public function testMainEntityOfPageSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsString( $dataSource->get_schema_property('mainEntityOfPage') );
+    }
+
+    public function testNameSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsString( $dataSource->get_schema_property('name') );
+    }
+
+    public function testPublisherSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertInstanceOf( Generic::class, $dataSource->get_schema_property('publisher') );
+    }
+
+    public function testTextSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsString( $dataSource->get_schema_property('text') );
+    }
+
+    public function testThumbnailUrlSchemaProp() {
+        $dataSource = $this->createPost( array('featured_image') );
+        $this->assertIsString( $dataSource->get_schema_property('thumbnailUrl') );
+    }
+
+    public function testWordCountSchemaProp() {
+        $dataSource = $this->createPost();
+        $this->assertIsNumeric( $dataSource->get_schema_property('wordCount') );
     }
 }
