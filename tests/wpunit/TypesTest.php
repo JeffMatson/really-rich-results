2575c29a23bd46d1577216fc9fc6aaf5909baa7f
JeffMatsonPagely <jeff.matson@pagely.com>
2021-06-16 18:44:35 -0400

    Tab fixes

--- tests/wpunit/TypesTest.php
+++ tests/wpunit/TypesTest.php
@@ -1,59 +1,63 @@
 <?php
 
-class TypesTest extends \Codeception\TestCase\WPTestCase
-{
-    /**
-     * @var \WpunitTester
-     */
-    protected $tester;
-    
-    public function setUp(): void
-    {
-        // Before...
-        parent::setUp();
+use Really_Rich_Results\Types;
+use Really_Rich_Results\Schema;
+use Really_Rich_Results\Data_Sources;
+use Really_Rich_Results\Content_Types;
 
-        // Your set up methods here.
-    }
+class TypesTest extends \Codeception\TestCase\WPTestCase {
+	/**
+	 * @var \WpunitTester
+	 */
+	protected $tester;
+	
+	public function setUp(): void
+	{
+		// Before...
+		parent::setUp();
 
-    public function tearDown(): void
-    {
-        // Your tear down methods here.
+		// Your set up methods here.
+	}
 
-        // Then...
-        parent::tearDown();
-    }
+	public function tearDown(): void
+	{
+		// Your tear down methods here.
 
-    // Tests
-    public function testIsSchemaObject() {
-        $schema_object = new \Really_Rich_Results\Schema\Thing();
+		// Then...
+		parent::tearDown();
+	}
 
-        // Check a valid schema object.
-        $this->assertEquals( \Really_Rich_Results\Types::is_schema_object( $schema_object ), true );
-        // Check an empty class.
-        $this->assertEquals( \Really_Rich_Results\Types::is_schema_object( new stdClass() ), false );
-        // Check a boolean
-        $this->assertEquals( \Really_Rich_Results\Types::is_schema_object( true ), false );
-    }
+	// Tests
+	public function testIsSchemaObject() {
+		$schema_object = new Schema\Thing();
 
-    public function testIsContentType() {
-        $content_type = new \Really_Rich_Results\Content_Types\Single();
+		// Check a valid schema object.
+		$this->assertTrue( true, Types::is_schema_object( $schema_object ) );
+		// Check an empty class.
+		$this->assertFalse( Types::is_schema_object( new stdClass() ) );
+		// Check a boolean
+		$this->assertFalse( Types::is_schema_object( true ) );
+	}
 
-        // Check an empty single content type.
-        $this->assertEquals( \Really_Rich_Results\Types::is_content_type( $content_type ), true );
-        // Check an empty class.
-        $this->assertEquals( \Really_Rich_Results\Types::is_content_type( new stdClass() ), false );
-        // Check a boolean
-        $this->assertEquals( \Really_Rich_Results\Types::is_content_type( true ), false );
-    }
+	public function testIsContentType() {
+		$content_type = new Content_Types\Single();
 
-    public function testIsDataSource() {
-        $data_source = new \Really_Rich_Results\Data_Sources\Generic();
+		// Check an empty single content type.
+		$this->assertTrue( Types::is_content_type( $content_type ) );
+		// Check an empty class.
+		$this->assertFalse( Types::is_content_type( new stdClass() ) );
+		// Check a boolean
+		$this->assertFalse( Types::is_content_type( true ) );
+	}
 
-        // Check a generic data source.
-        $this->assertEquals( \Really_Rich_Results\Types::is_data_source( $data_source ), true );
-        // Check an empty class.
-        $this->assertEquals( \Really_Rich_Results\Types::is_data_source( new stdClass() ), false );
-        // Check a boolean
-        $this->assertEquals( \Really_Rich_Results\Types::is_data_source( true ), false );
-    }
+	public function testIsDataSource() {
+		$data_source = new Data_Sources\Generic();
+
+		// Check a generic data source.
+		$this->assertTrue( Types::is_data_source( $data_source ) );
+		// Check an empty class.
+		$this->assertFalse( Types::is_data_source( new stdClass() ) );
+		// Check a boolean
+		$this->assertFalse( Types::is_data_source( true ) );
+	}
 }
